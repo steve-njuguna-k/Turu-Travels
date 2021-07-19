5d0637beff8a9f679a326da6aff0726f3aa96da7
Steve Njuguna <stevenjuguna93@gmail.com>
2021-07-19 16:14:41 +0300

    Added dummy content to the home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ToursComponent } from './components/tours/tours.component';
+import { HotelsComponent } from './components/hotels/hotels.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FavouritesComponent } from './components/favourites/favourites.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ToursComponent,
+    HotelsComponent,
+    ContactComponent,
+    FavouritesComponent
   ],
   imports: [
     BrowserModule,
