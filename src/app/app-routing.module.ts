4a705b4e6ac121b80fb3c5a90642ba2c62c694bd
Steve Njuguna <stevenjuguna93@gmail.com>
2021-07-19 20:32:27 +0300

    Added dummy text to Categories page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+import { ToursComponent } from './components/tours/tours.component';
+import { HotelsComponent } from './components/hotels/hotels.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FavouritesComponent } from './components/favourites/favourites.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { TourDetailsComponent } from './components/tour-details/tour-details.component';
+import { HotelDetailsComponent } from './components/hotel-details/hotel-details.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'blog', component: BlogComponent},
+  {path: 'categories', component: CategoriesComponent},
+  {path: 'contact-us', component: ContactComponent},
+  {path: 'hotels', component: HotelsComponent},
+  {path: 'tours', component: ToursComponent},
+  {path: 'featured', component: FavouritesComponent},
+  {path: '**', component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [
+  HomeComponent,
+  BlogComponent,
+  CategoriesComponent,
+  ContactComponent,
+  HotelsComponent,
+  ToursComponent,
+  FavouritesComponent
+]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,44 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule , routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
-import { ToursComponent } from './components/tours/tours.component';
-import { HotelsComponent } from './components/hotels/hotels.component';
-import { ContactComponent } from './components/contact/contact.component';
-import { FavouritesComponent } from './components/favourites/favourites.component';
-import { BlogComponent } from './components/blog/blog.component';
-import { TourDetailsComponent } from './components/tour-details/tour-details.component';
-import { HotelDetailsComponent } from './components/hotel-details/hotel-details.component';
-import { CategoriesComponent } from './components/categories/categories.component';
+
 import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    ToursComponent,
-    HotelsComponent,
-    ContactComponent,
-    FavouritesComponent,
-    BlogComponent,
-    TourDetailsComponent,
-    HotelDetailsComponent,
-    CategoriesComponent
+    routingComponents,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot([
-      {path: 'home', component: HomeComponent},
-      {path: 'blog', component: BlogComponent},
-      {path: 'categories', component: CategoriesComponent},
-      {path: 'contact-us', component: ContactComponent},
-      {path: 'hotels', component: HotelsComponent},
-      {path: 'tours', component: ToursComponent},
-      {path: 'featured', component: FavouritesComponent},
-    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
