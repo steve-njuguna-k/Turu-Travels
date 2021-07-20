1fda5b7d3cb0006caf7456eac87bf1e4c2e8f397
Steve Njuguna <stevenjuguna93@gmail.com>
2021-07-20 03:55:11 +0300

    Added a 404 page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 
 import { RouterModule } from '@angular/router';
 import { BlogDetailsComponent } from './components/blog-details/blog-details.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     routingComponents,
     BlogDetailsComponent,
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
